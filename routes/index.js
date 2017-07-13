3f62a5faf435f796eb5ebf3755e858e38c006fea
于洋 <yuyang@yuyangdeMBP.fios-router.home>
2017-07-13 14:42:43 -0400

    merge

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', { biaoqian: 'yu' });
-=======
   res.render('index', {partials:{
 		header:'../views/partials/header.hjs',
 		navbar:'../views/partials/navbar.hjs',
 		footer:'../views/partials/footer.hjs',
 	}});
->>>>>>> 71465bf9f45713cc904df09e09f47ec628e0fd27
 });
 
 module.exports = router;
